720049d304350b48dde720058eb13143a8ab42e0
dreamheart <leondreamheart@outlook.com>
2021-07-23 12:34:03 -0400

    Added routes for get notes, and save notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,43 @@
 const path = require("path");
 const fs = require('fs');
 const util = require('util');
+const { readAndAppend } = require('../helpers/fsUtils');
 
 const router = require("express").Router();
 
-// Promise version of fs.readFile
+// change to be called from fsUtils folder
 const readFromFile = util.promisify(fs.readFile);
 
-router.get("notes", (req, res) => {
+// define the home page route
+router.get("/notes", (req, res) => {
     console.info(`${req.method} request received for the notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => {
+        console.log(data);
+        res.json(JSON.parse(data));
+    });
+});
+
+router.post("/notes", (req, res) => {
+    console.log(req.body);
+    const {title, text} = req.body;
+
+    if(title && text){
+    const newNote = {
+        title,
+        text
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+        status: "sucess",
+        body: newNote
+    };
+
+    res.json(response);
+} else{
+    res.json("Error in posting note");
+}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,9 +9,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use("/", htmlRoutes);
-app.use("/", apiRoutes);
 
-apiRoutes.get("notes");
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
